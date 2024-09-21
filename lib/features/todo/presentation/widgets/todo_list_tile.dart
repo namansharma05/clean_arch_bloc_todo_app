87b310dcbe32f782cb9ffbad3e3b79dcf39e3dc8
Naman Sharma <namanks0511@gmail.com>
2024-09-21 20:05:45 +0530

    added updateTodo and deleteTodo functionalities, project completion

--- lib/features/todo/presentation/widgets/todo_list_tile.dart
+++ lib/features/todo/presentation/widgets/todo_list_tile.dart
@@ -1,5 +1,6 @@
 import 'package:clean_arch_bloc_todo_app/features/todo/domain/entities/todo_entity.dart';
 import 'package:clean_arch_bloc_todo_app/features/todo/presentation/bloc/todo_bloc.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TodoListTile extends StatelessWidget {
@@ -9,18 +10,40 @@ class TodoListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Checkbox(
-          value: todo.isCompleted,
-          onChanged: (value) {
-            final todoEntity = TodoEntity(
-                id: todo.id, title: todo.title, isCompleted: !todo.isCompleted);
-            todoBloc.add(TodoUpdateEvent(todoEntity: todoEntity));
-          }),
-      title: Text(
-        todo.title,
-        style:
-            const TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 5),
+      child: ListTile(
+        tileColor: Colors.grey.shade500,
+        leading: Checkbox(
+            activeColor: Colors.grey.shade700,
+            value: todo.isCompleted,
+            onChanged: (value) {
+              final todoEntity = TodoEntity(
+                  id: todo.id,
+                  title: todo.title,
+                  isCompleted: !todo.isCompleted);
+              todoBloc.add(TodoUpdateEvent(todoEntity: todoEntity));
+            }),
+        title: Text(
+          todo.title,
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            decoration: todo.isCompleted
+                ? TextDecoration.lineThrough
+                : TextDecoration.none,
+            decorationColor: Colors.black,
+            decorationThickness: 3,
+          ),
+        ),
+        trailing: IconButton(
+            onPressed: () {
+              todoBloc.add(TodoDeleteEvent(todoEntity: todo));
+            },
+            icon: const Icon(
+              CupertinoIcons.delete_simple,
+              color: Colors.black,
+            )),
       ),
     );
   }
